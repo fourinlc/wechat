3b3a52fe66c6060ee1a11c4e97949572381a7061
lan.lin <lan.lin@hairoutech.com>
2022-07-15 17:18:23 +0800

    Initial commit

---
+++ wechat-server/src/main/java/com/xxx/server/wechatApplication.java
@@ -0,0 +1,19 @@
+package com.xxx.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @PackageName:com.xxx.server
+ * @ClassName:wechatApplication Description:
+ * @author: lc
+ * @date 2022/7/14 17:57
+ */
+@SpringBootApplication
+@MapperScan("com.xxx.server.mapper")
+public class wechatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(wechatApplication.class,args);
+    }
+}
