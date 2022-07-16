14122687ca0c6cf7dcc36f2fa080626c06dccb14
qujian <632959801@qq.com>
2022-07-16 09:53:34 +0800

    test

--- wechat-server/src/main/java/com/xxx/server/wechatApplication.java
+++ wechat-server/src/main/java/com/xxx/server/wechatApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.xxx.server.mapper")
 public class wechatApplication {
     public static void main(String[] args) {
+        //
         SpringApplication.run(wechatApplication.class,args);
     }
 }
