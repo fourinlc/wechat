1a15822bd6e1be18989f10a6c0bcb811f6a3f3b4
jianqu001 <632959801>
2022-08-10 08:58:57 +0800

    xxx

---
+++ wechat-server/src/main/java/com/xxx/server/service/IWeixinTemplateDetailService.java
@@ -0,0 +1,16 @@
+package com.xxx.server.service;
+
+import com.xxx.server.pojo.WeixinTemplateDetail;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lc
+ * @since 2022-08-10
+ */
+public interface IWeixinTemplateDetailService extends IService<WeixinTemplateDetail> {
+
+}
