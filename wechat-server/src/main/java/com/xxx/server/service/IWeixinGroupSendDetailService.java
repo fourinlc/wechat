079120b54644d9d223435c30acd94205bd6bf96c
jianqu001 <632959801>
2022-08-30 08:48:13 +0800

    微信日志以及缺失文件信息

---
+++ wechat-server/src/main/java/com/xxx/server/service/IWeixinGroupSendDetailService.java
@@ -0,0 +1,16 @@
+package com.xxx.server.service;
+
+import com.xxx.server.pojo.WeixinGroupSendDetail;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lc
+ * @since 2022-08-30
+ */
+public interface IWeixinGroupSendDetailService extends IService<WeixinGroupSendDetail> {
+
+}
--- wechat-server/src/main/java/com/xxx/server/service/impl/WeixinLogServiceImpl.java
+++ wechat-server/src/main/java/com/xxx/server/service/impl/WeixinLogServiceImpl.java
@@ -1,9 +1,9 @@
 package com.xxx.server.service.impl;
 
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xxx.server.mapper.WeixinLogMapper;
 import com.xxx.server.pojo.WeixinLog;
+import com.xxx.server.service.IWeixinLogService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +15,6 @@ import org.springframework.stereotype.Service;
  * @since 2022-08-29
  */
 @Service
-public class WeixinLogServiceImpl extends ServiceImpl<WeixinLogMapper, WeixinLog> implements IService<WeixinLog> {
+public class WeixinLogServiceImpl extends ServiceImpl<WeixinLogMapper, WeixinLog> implements IWeixinLogService {
 
 }
