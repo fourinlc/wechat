c5ec568a264656d8de4602cc59f0adecec1265ab
lan.lin <lan.lin@hairoutech.com>
2022-09-02 17:37:50 +0800

    修改线程数

--- wechat-server/src/main/java/com/xxx/server/config/ThreadConfig.java
+++ wechat-server/src/main/java/com/xxx/server/config/ThreadConfig.java
@@ -18,9 +18,9 @@ public class ThreadConfig {
     public ThreadPoolTaskExecutor executor(){
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         //配置核心线程数
-        executor.setCorePoolSize(15);
+        executor.setCorePoolSize(100);
         //配置最大线程数
-        executor.setMaxPoolSize(30);
+        executor.setMaxPoolSize(175);
         //配置队列大小
         executor.setQueueCapacity(1000);
         //线程的名称前缀
