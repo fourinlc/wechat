5c9a1b3dc58a364ef483951d3b2e6a1a2f70e070
jianqu001 <632959801>
2022-09-19 15:44:13 +0800

    增加空值校验

--- wechat-server/src/main/java/com/xxx/server/service/impl/WeixinTemplateServiceImpl.java
+++ wechat-server/src/main/java/com/xxx/server/service/impl/WeixinTemplateServiceImpl.java
@@ -208,7 +208,9 @@ public class WeixinTemplateServiceImpl extends ServiceImpl<WeixinTemplateMapper,
         wxIds.removeIf(wxId -> {
             WeixinBaseInfo weixinBaseInfo = weixinBaseInfoService.getById(wxId);
             // 为空的情况下说明选择的wxId有误
-            Assert.notNull(weixinBaseInfo, "该微信id有误:" + wxId);
+            if(StrUtil.isNotEmpty(wxId)){
+                Assert.notNull(weixinBaseInfo, "该微信id有误:" + wxId);
+            }
             return !(StrUtil.isNotEmpty(weixinBaseInfo.getKey()) && StrUtil.equals(weixinBaseInfo.getState(), "1"));
         });
         JSONObject result = JSONObject.of("code", 200, "msg", "群发送消息成功");
